17a4ca6538c96d31fe9179b58b5df35eea019438
joeynovak <joey.novak@gmail.com>
2019-10-05 12:31:49 -0700

    Lulu - File had wrong case

--- src/NovakSolutions/LuluPhpSdk/Service/LuluService.php
+++ src/NovakSolutions/LuluPhpSdk/Service/LuluService.php
@@ -7,7 +7,7 @@ namespace NovakSolutions\LuluPhpSdk\Service;
 use NovakSolutions\LuluPhpSdk\Lulu;
 use NovakSolutions\RestSdkBase\Service\Service;
 
-class LuLuService extends Service
+class LuluService extends Service
 {
     public static $webRequesterRegistryKeyPrefix = Lulu::LULU_REGISTRY_PREFIX;
     public static $arrayKey = 'results';
